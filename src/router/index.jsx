a0791805b8ad021aaa33ead0eb94735a96bfa063
goodtimes0504 <revolution_lc@outlook.com>
2024-12-30 20:15:37 +0800

    feat:路径@别名 和联想 配置

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,6 @@
 //路由配置
-import Layout from "../pages/Layout"
-import Login from "../pages/Login"
+import Layout from "@/pages/Layout"
+import Login from "@/pages/Login"
 import { createBrowserRouter } from "react-router-dom"
 
 const router = createBrowserRouter(
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import { fileURLToPath } from "url"
+import path from "path"
+import react from "@vitejs/plugin-react"
+
+// 定义 __dirname
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
